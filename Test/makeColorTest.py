13fe8de9bc16eb1a28e6c1de946c4dac6d93e384
PabloPalaciosAlonso <pablo.pal@hotmail.com>
2023-08-10 18:53:40 +0200

    Add screenshot available colors

--- Test/makeColorTest.py
+++ Test/makeColorTest.py
@@ -1,5 +1,5 @@
 numberColors = 65
 file = open("Colors.svv", "w")
 for i in range(numberColors):
-    file.write(str(3*(i%13))+" "+str(3*int(i/13))+" 0 1 "+str(i)+"\n")
+    file.write(str(3*(i%13))+" "+str(-3*int(i/13))+" 0 1 "+str(i)+"\n")
 file.close()
